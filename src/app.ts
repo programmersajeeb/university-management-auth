bb86ad918ccd46449cec407c0da2d8c314b3a80f
Shajeeb <liveclasswd@gmail.com>
2023-07-16 12:12:31 +0600

    setup Eslint, prettier, husky, lint stage by using RocketLoger blog

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-import cors from "cors";
-const port = 4000;
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
+// const port = 4000;
 
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(`khela shuru`);
-});
+app.get('/', (req: Request, res: any) => {
+  res.send(`khela shuru`)
+})
 
-export default app;
+export default app
