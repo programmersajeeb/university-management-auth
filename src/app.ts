e94fce3b549b667abc6ad15fb276bf163b35c064
Shajeeb <liveclasswd@gmail.com>
2023-07-17 15:48:04 +0600

    add some route

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
+import usersRoute from './app/modules/users/users.route'
 // const port = 4000;
 
 app.use(cors())
+
+// application route
+app.use('/api/v1/users/', usersRoute)
 
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send(`khela shuru`)
 })
 
